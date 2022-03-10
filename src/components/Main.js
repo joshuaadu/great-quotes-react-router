a71f119c2d1acf5f51124a59cf74ee6f3622762b
Joshua Adu-Mensah (Qwaben Josh) <joshuaadu2001@gmail.com>
2022-03-10 06:53:23 +0000

    add dummy quotes data

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,13 +2,28 @@ import { Route } from "react-router-dom";
 import classes from "./layout/Layout.module.css";
 import QuoteList from "./quotes/QuoteList";
 import QuoteForm from "./quotes/QuoteForm";
+import NoQuotesFound from "./quotes/NoQuotesFound";
 
 const Main = (props) => {
+  const quotes = [
+    {
+      key: "1",
+      id: "1",
+      author: "author1",
+      text: "text1"
+    },
+    {
+      key: "2",
+      id: "2",
+      author: "author2",
+      text: "text2"
+    }
+  ];
+  const allQuotesContent =
+    quotes.length > 0 ? <QuoteList quotes={quotes} /> : <NoQuotesFound />;
   return (
     <main className={classes.main}>
-      <Route path="/all-quotes">
-        <QuoteList />
-      </Route>
+      <Route path="/all-quotes">{allQuotesContent}</Route>
       <Route path="/add-quotes">
         <QuoteForm />
       </Route>
