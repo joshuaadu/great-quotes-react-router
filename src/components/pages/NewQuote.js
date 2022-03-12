3cccbe1b9c079c036b1ab4e18877bba3dbf9aff9
Joshua Adu-Mensah (Qwaben Josh) <joshuaadu2001@gmail.com>
2022-03-12 13:10:37 +0000

    add new quote to QuoteData context

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,8 +1,15 @@
 import { useContext } from "react";
 import QuoteForm from "../quotes/QuoteForm";
+import { QuoteContext } from "../store/quotes-data";
 
 const NewQuote = (props) => {
-  return <QuoteForm />;
+  const { addQuote } = useContext(QuoteContext);
+
+  const addQuoteHandler = (quote) => {
+    addQuote(quote);
+  };
+
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
