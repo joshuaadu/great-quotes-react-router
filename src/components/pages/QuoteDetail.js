08ddb5cd1304cdb47f6845a8562d5122a757b0cf
Joshua Adu-Mensah (Qwaben Josh) <joshuaadu2001@gmail.com>
2022-03-15 06:52:30 +0000

    add Highlighted Quote component

--- src/components/pages/QuoteDetail.js
+++ src/components/pages/QuoteDetail.js
@@ -1,10 +1,19 @@
+import { useContext, useMemo } from "react";
 import { Route, useParams } from "react-router-dom";
+import HighlightedQuote from "../quotes/HighlightedQuote";
 import Comments from "../comments/Comments";
+import { QuoteContext } from "../store/quotes-data";
 const QuoteDetail = (props) => {
   const { quoteId } = useParams();
+  const { quotes } = useContext(QuoteContext);
 
+  const quotesMemoized = useMemo(() => quotes, [quotes]);
+  console.log("quotes", quotesMemoized, quoteId);
+  const quote = quotesMemoized.find((quote) => quote.id == quoteId);
+  console.log("quotes", quotesMemoized, quote);
   return (
     <>
+      {/* <HighlightedQuote text={text} author={author} /> */}
       <p>{quoteId}</p>
       <Route path="/quotes/:quoteId/comments">
         <Comments />
