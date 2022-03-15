2abf4d3110e521469073e99c2aa14d2d2bbf8c6f
Joshua Adu-Mensah (Qwaben Josh) <joshuaadu2001@gmail.com>
2022-03-15 06:59:51 +0000

    add page not found

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -4,6 +4,7 @@ import AllQuotes from "../pages/AllQuotes";
 import NewQuote from "../pages/NewQuote";
 import QuoteDetail from "../pages/QuoteDetail";
 import QuoteDataProvider from "../store/quotes-data";
+import PageNotFound from "../pages/PageNotFound";
 
 const Main = (props) => {
   return (
@@ -21,6 +22,9 @@ const Main = (props) => {
           </Route>
           <Route path="/new-quotes">
             <NewQuote />
+          </Route>
+          <Route path="*">
+            <PageNotFound />
           </Route>
         </Switch>
       </main>
