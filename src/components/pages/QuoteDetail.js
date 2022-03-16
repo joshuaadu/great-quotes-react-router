7334f9fddcf98d6be09a7dc7f987e826529d527c
Joshua Adu-Mensah (Qwaben Josh) <joshuaadu2001@gmail.com>
2022-03-16 19:50:23 +0000

    load comments conditionally using the power of Routing This is simple, clean, and elegant

--- src/components/pages/QuoteDetail.js
+++ src/components/pages/QuoteDetail.js
@@ -1,4 +1,4 @@
-import { useContext, useMemo, useState } from "react";
+import { useContext, useMemo } from "react";
 import { Link, Route, useParams } from "react-router-dom";
 import HighlightedQuote from "../quotes/HighlightedQuote";
 import Comments from "../comments/Comments";
@@ -6,33 +6,24 @@ import { QuoteContext } from "../store/quotes-data";
 const QuoteDetail = (props) => {
   const { quoteId } = useParams();
   const { quotes } = useContext(QuoteContext);
-  const [showComments, setShowComments] = useState(false);
 
   const quotesMemoized = useMemo(() => quotes, [quotes]);
-  console.log("quotes", quotesMemoized, quoteId);
   const quote = quotesMemoized.find((quote) => quote.id === quoteId);
-  console.log("quote", quote);
 
-  const showCommentHandler = () => {
-    setShowComments(true);
-  };
   return (
     <>
       <HighlightedQuote text={quote?.text} author={quote?.author} />
-      <div className="centered">
-        {!showComments && (
-          <Link to={`/quotes/${quoteId}/comments`}>
-            <button onClick={showCommentHandler} className="btn--flat">
-              Show Comments
-            </button>
+      <Route path={`/quotes/${quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${quoteId}/comments`}>
+            Show Comments
           </Link>
-        )}
-      </div>
-      {showComments && (
-        <Route path={`/quotes/${quoteId}/comments`}>
-          <Comments />
-        </Route>
-      )}
+        </div>
+      </Route>
+
+      <Route path={`/quotes/${quoteId}/comments`}>
+        <Comments />
+      </Route>
     </>
   );
 };
